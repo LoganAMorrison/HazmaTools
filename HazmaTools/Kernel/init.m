ea04c2009b0839ed8dadb6db7127100722795aef
Logan A Morrison <loanmorr@ucsc.com>
2019-11-16 17:12:53 -0800

    Adding Rambo functionallity for testing spectra functions

--- HazmaTools/Kernel/init.m
+++ HazmaTools/Kernel/init.m
@@ -1,22 +1,23 @@
 (* ::Package:: *)
 
-(* Mathematica Init File *)  
+(* Mathematica Init File *)
 
-Global`$LoadFeynArts=True;
+Global`$LoadFeynArts = True;
 Needs["FeynCalc`"];
-$FAVerbose=False;
+$FAVerbose = False;
 
 BeginPackage["HazmaTools`"];
 
 Print["MeV-DM - Scalar Mediator: version 0.2.1"];
 Print["by Adam Coogan and Logan A. Morrison"];
 
-Get[FileNameJoin[{DirectoryName[$InputFileName,2], "Utilities.m"}]];
-Get[FileNameJoin[{DirectoryName[$InputFileName,2], "States.m"}]];
-Get[FileNameJoin[{DirectoryName[$InputFileName,2], "Diagrams.m"}]];
-Get[FileNameJoin[{DirectoryName[$InputFileName,2], "Amplitudes.m"}]];
-Get[FileNameJoin[{DirectoryName[$InputFileName,2], "CrossSections.m"}]];
-Get[FileNameJoin[{DirectoryName[$InputFileName,2], "Widths.m"}]];
-Get[FileNameJoin[{DirectoryName[$InputFileName,2], "Spectra.m"}]];
+Get[FileNameJoin[{DirectoryName[$InputFileName, 2], "Utilities.m"}]];
+Get[FileNameJoin[{DirectoryName[$InputFileName, 2], "States.m"}]];
+Get[FileNameJoin[{DirectoryName[$InputFileName, 2], "Diagrams.m"}]];
+Get[FileNameJoin[{DirectoryName[$InputFileName, 2], "Amplitudes.m"}]];
+Get[FileNameJoin[{DirectoryName[$InputFileName, 2], "CrossSections.m"}]];
+Get[FileNameJoin[{DirectoryName[$InputFileName, 2], "Widths.m"}]];
+Get[FileNameJoin[{DirectoryName[$InputFileName, 2], "Spectra.m"}]];
+Get[FileNameJoin[{DirectoryName[$InputFileName, 2], "Rambo.m"}]];
 
 EndPackage[];
